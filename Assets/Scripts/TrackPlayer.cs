2462554f5d31b87342b83599361c5f2d0c3a93ee
S-Thoms <50848416+S-Thoms@users.noreply.github.com>
2019-05-30 01:28:53 +0100

    I don't know what I'm doing.
    What is this?

--- Assets/Scripts/TrackPlayer.cs
+++ Assets/Scripts/TrackPlayer.cs
@@ -2,18 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class TrackPlayer : MonoBehaviour
 {
+    Camera currCamera;
     public float interpVelocity;
     public float minDistance;
     public float followDistance;
     public GameObject target;
     public Vector3 offset;
     Vector3 targetPos;
+    public float wheelScroll;
+    public float defFieldOfView = 60, minFieldOfView = 0, maxFieldOfView = 120;
     // Use this for initialization
     void Start()
     {
         targetPos = transform.position;
+        currCamera = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        
+        wheelScroll = Input.GetAxis("Mouse ScrollWheel");
+
     }
 
     // Update is called once per frame
@@ -32,6 +44,12 @@ public class TrackPlayer : MonoBehaviour
 
             transform.position = Vector3.Lerp(transform.position, targetPos + offset, 0.25f);
 
+            currCamera.fieldOfView -= wheelScroll;
+
+            if (currCamera.fieldOfView < minFieldOfView)
+                currCamera.fieldOfView = minFieldOfView;
+            if (currCamera.fieldOfView > maxFieldOfView)
+                currCamera.fieldOfView = maxFieldOfView;
         }
     }
 }
