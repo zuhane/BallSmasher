9f5c0c1b7167ccebd5399621016085abf35707b6
Tom Kingston <zuhane1@hotmail.co.uk>
2020-01-06 18:10:40 +0000

    Membrane Ball improvements

--- Assets/Scripts/Balls/MembraneBall.cs
+++ Assets/Scripts/Balls/MembraneBall.cs
@@ -5,6 +5,13 @@ public class MembraneBall : MonoBehaviour
 {
     private Rigidbody2D rigid;
     private Timer timer;
+    [SerializeField] private float consumeTime = 1.5f;
+    [SerializeField] private int disableTime = 300;
+    private bool disabled;
+    private int disabledCounter;
+    private Animator animator;
+
+    private SpriteRenderer spriteRend;
 
     public class CapturedPlayer
     {
@@ -19,15 +26,27 @@ public class MembraneBall : MonoBehaviour
     {
         rigid = GetComponent<Rigidbody2D>();
         timer = Timer.CreateComponent(gameObject);
+        spriteRend = GetComponentInChildren<SpriteRenderer>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timer.SecondsPassed(0.5f))
+        //TODO: Add second timer in. Can't have 2 of the same component?!?!?! Hmmmm...
+        if (disabled)
+        {
+            disabledCounter++;
+
+            if (disabledCounter >= disableTime)
+            EnableSlurp();
+        }
+
+        if (timer.SecondsPassed(0.5f) && capturedPlayers.Count < 1)
         {
             rigid.AddForce(VectorMath.Random() * Random.Range(6, 12));
         }
+
         foreach (CapturedPlayer capturedPlayer in capturedPlayers)
         {
             if (capturedPlayer.timer.LimitReached())
@@ -37,10 +56,25 @@ public class MembraneBall : MonoBehaviour
         }
     }
 
+    private void EnableSlurp()
+    {
+        disabledCounter = 0;
+        disabled = false;
+        animator.SetBool("Dormant", disabled);
+        animator.SetTrigger("Wobble");
+    }
+
+    private void DisableSlurp()
+    {
+        disabled = true;
+        animator.SetBool("Dormant", disabled);
+        animator.SetTrigger("Wobble");
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         GameObject goCollisionRoot = collision.transform.root.gameObject;
-        if (goCollisionRoot.tag == "Player")
+        if (goCollisionRoot.tag == "Player" && !disabled)
         {
             consumePlayer(goCollisionRoot);
         }
@@ -49,6 +83,9 @@ public class MembraneBall : MonoBehaviour
 
     private void consumePlayer(GameObject player)
     {
+        rigid.velocity = Vector2.zero;
+        AudioManager.PlaySound("MembraneRelease", UnityEngine.Random.Range(0.8f, 1.2f));
+        Instantiate(Resources.Load<GameObject>("Effects/MembraneSplat"), transform.position, Quaternion.identity, transform);
         player.transform.parent = transform;
         player.transform.localPosition = new Vector3(0, 0, 0);
         foreach (Rigidbody2D rigid in player.GetComponentsInChildren<Rigidbody2D>())
@@ -59,17 +96,22 @@ public class MembraneBall : MonoBehaviour
 
         CapturedPlayer capturedPlayer = new CapturedPlayer();
         capturedPlayer.player = player;
-        capturedPlayer.timer = Timer.CreateComponent(gameObject, 3);
+        capturedPlayer.timer = Timer.CreateComponent(gameObject, (int)consumeTime);
         capturedPlayers.Add(capturedPlayer);
     }
 
     public void releasePlayer(CapturedPlayer cp)
     {
+        AudioManager.PlaySound("MembraneRelease", UnityEngine.Random.Range(0.8f, 1.2f));
+        Instantiate(Resources.Load<GameObject>("Effects/MembraneSplat"), transform.position, Quaternion.identity, transform);
+        DisableSlurp();
         cp.player.transform.parent = null;
         foreach (Rigidbody2D rigid in cp.player.GetComponentsInChildren<Rigidbody2D>())
         {
             cp.player.gameObject.GetComponent<Rigidbody2D>().simulated = true;
         }
+
+        cp.player.transform.rotation = new Quaternion(0, 0, 0, 0);
         cp.player.GetComponent<PlayerPhysicsMovement>().enabled = true;
         capturedPlayers.Remove(cp);
     }
